454327eb3964154d28231e8633ca76370b61feee
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-10-31 14:28:25 -0500

    Update skeleton code

--- iTrust/src/edu/ncsu/csc/itrust/action/SendReminderAction.java
+++ iTrust/src/edu/ncsu/csc/itrust/action/SendReminderAction.java
@@ -1,11 +1,28 @@
 package edu.ncsu.csc.itrust.action;
 
 import java.sql.SQLException;
+
 import edu.ncsu.csc.itrust.beans.ApptBean;
+import edu.ncsu.csc.itrust.beans.PatientBean;
+import edu.ncsu.csc.itrust.beans.PersonnelBean;
+import edu.ncsu.csc.itrust.dao.DAOFactory;
+import edu.ncsu.csc.itrust.dao.mysql.PatientDAO;
+import edu.ncsu.csc.itrust.dao.mysql.PersonnelDAO;
 import edu.ncsu.csc.itrust.exception.FormValidationException;
 import edu.ncsu.csc.itrust.exception.ITrustException;
 
 public class SendReminderAction {
+    private long loggedInMID;
+    private PatientDAO patientDAO;
+    private PersonnelDAO personnelDAO;
+
+    public SendReminderAction(DAOFactory factory, long loggedInMID) {
+        this.loggedInMID = loggedInMID;
+        this.patientDAO = factory.getPatientDAO();
+        this.personnelDAO = factory.getPersonnelDAO();
+    }
+
     public void sendReminder(ApptBean aBean) throws ITrustException, SQLException, FormValidationException {
+        
     }
 }
