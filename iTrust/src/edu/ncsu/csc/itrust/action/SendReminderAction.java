0aa8d7144b19076cdd6664551842dcc07b1e78a6
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-10-30 20:53:28 -0500

    Add UC41 skeleton code

---
+++ iTrust/src/edu/ncsu/csc/itrust/action/SendReminderAction.java
@@ -0,0 +1,11 @@
+package edu.ncsu.csc.itrust.action;
+
+import java.sql.SQLException;
+import edu.ncsu.csc.itrust.beans.ApptBean;
+import edu.ncsu.csc.itrust.exception.FormValidationException;
+import edu.ncsu.csc.itrust.exception.ITrustException;
+
+public class SendReminderAction {
+    public void sendReminder(ApptBean aBean) throws ITrustException, SQLException, FormValidationException {
+    }
+}
