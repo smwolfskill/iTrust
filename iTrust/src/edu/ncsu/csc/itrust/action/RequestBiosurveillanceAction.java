63032714b74d655ede30eeec5948b3d623eb64fe
Sean <Sean@wirelessprv-10-193-97-218.near.illinois.edu>
2017-11-09 13:40:35 -0600

    sanity check done

--- iTrust/src/edu/ncsu/csc/itrust/action/RequestBiosurveillanceAction.java
+++ iTrust/src/edu/ncsu/csc/itrust/action/RequestBiosurveillanceAction.java
@@ -2,7 +2,9 @@ package edu.ncsu.csc.itrust.action;
 
 import edu.ncsu.csc.itrust.dao.DAOFactory;
 import edu.ncsu.csc.itrust.dao.mysql.DiagnosesDAO;
+import edu.ncsu.csc.itrust.exception.FormValidationException;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class RequestBiosurveillanceAction {
@@ -19,7 +21,29 @@ public class RequestBiosurveillanceAction {
     * date: the date the user typed in
     */
     public String detectEpidemic(String icdCode, String zipCode, Date date) {
+
+        /* Check zip code validation */
+        try{
+            if (!zipCode.matches("([0-9]{5})|([0-9]{5}-[0-9]{4})"))
+                throw new FormValidationException("invalid zip code");
+            if (Integer.valueOf(zipCode)<10000 || Integer.valueOf(zipCode)>99999)
+                throw new FormValidationException("length should exactly equal to 5");
+        } catch (Exception e) { return "invalid zip code"; }
+
+        /* Check diagnosis code validation */
+        try {
+            if (!(icdCode.equals("84.50") || icdCode.equals("487.00")) ) {      //not sure about icd Code yet
+                throw new FormValidationException("Exception");
+            }
+        } catch (Exception e) { return "invalid diagnosis code"; }
+
         return null;
+    }
+
+    /* return the order of the week of the date from 1/1 in same year */
+    public long weekNumber(Date date) {
+        Date startDateOfYear = new Date(date.getYear(),1,1);
+        return (date.getTime() - startDateOfYear.getTime())/(1000 * 60 * 60 * 24 * 7) + 1;
     }
 
     /*
--- iTrust/test/edu/ncsu/csc/itrust/unit/action/RequestBiosurveillanceActionTest.java
+++ iTrust/test/edu/ncsu/csc/itrust/unit/action/RequestBiosurveillanceActionTest.java
@@ -1,16 +1,37 @@
 package edu.ncsu.csc.itrust.unit.action;
 
+import edu.ncsu.csc.itrust.action.RequestBiosurveillanceAction;
+import edu.ncsu.csc.itrust.dao.DAOFactory;
 import edu.ncsu.csc.itrust.unit.datagenerators.TestDataGenerator;
+import edu.ncsu.csc.itrust.unit.testutils.TestDAOFactory;
 import junit.framework.TestCase;
+
+import java.util.Date;
 
 public class RequestBiosurveillanceActionTest extends TestCase {
 
     private TestDataGenerator gen;
+    private RequestBiosurveillanceAction action;
+    private DAOFactory factory = TestDAOFactory.getTestInstance();
+
 
     protected void setUp() throws Exception {
         super.setUp();
         gen = new TestDataGenerator();
         gen.clearAllTables();
         gen.standardData();
+        action = new RequestBiosurveillanceAction(factory);
+    }
+
+    public void testDetectEpidemic_ZipCode() {
+        assertEquals("invalid zip code", action.detectEpidemic("anything", "ABCDE", new Date()));
+        assertEquals("invalid zip code", action.detectEpidemic("anything", "1234", new Date()));
+        assertEquals("invalid zip code", action.detectEpidemic("anything", "01234", new Date()));
+        assertEquals("invalid zip code", action.detectEpidemic("anything", "1A234", new Date()));
+        assertEquals(null, action.detectEpidemic("84.50", "13234", new Date()));
+    }
+
+    public void testWeekNumber() {
+        assertEquals(45, action.weekNumber(new Date(2017,11,9)));
     }
 }
