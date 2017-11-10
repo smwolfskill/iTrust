69eb557a6cebc7429bdf7e6d1d004cc6b8aed20f
wolfski2 <wolfski2@illinois.edu>
2017-11-09 23:10:09 -0600

    finished UC14_1

--- iTrust/src/edu/ncsu/csc/itrust/action/RequestBiosurveillanceAction.java
+++ iTrust/src/edu/ncsu/csc/itrust/action/RequestBiosurveillanceAction.java
@@ -12,17 +12,22 @@ import java.util.Date;
 
 public class RequestBiosurveillanceAction {
     DiagnosesDAO diagDAO;
+    private final long DAY = 1000*60*60*24L;
 
     public RequestBiosurveillanceAction (DAOFactory factory){
         diagDAO = factory.getDiagnosesDAO();
     }
 
-    /*
-    * return: "Yes", "No", "No analysis can occur.", "invalid diagnosis code", "invalid zip code", or "invalid date"
-    * icdCode: the diagnose code that the user typed in
-    * zipCode: the zip code that the user typed in
-    * date: the date the user typed in
-    */
+    /**
+     * Detect an epidemic.
+     * @param icdCode Code of the epidemic.
+     * @param zipCode zip code to consider local region/area of.
+     * @param date Date to check for epidemic within two weeks prior to.
+     * @param threshold If applicable, proportion to apply: 1.0 is normal, 0.5 is twice as likely to report epidemic.
+     * @return "Yes", "No", "No analysis can occur.", "invalid diagnosis code", "invalid zip code", or "invalid date"
+     * @throws ITrustException
+     * @throws FormValidationException
+     */
     public String detectEpidemic(String icdCode, String zipCode, Date date, Double threshold) throws ITrustException, FormValidationException {
 
         ArrayList<DiagnosisStatisticsBean> diagnosisStatisticsBean = null;
@@ -31,7 +36,7 @@ public class RequestBiosurveillanceAction {
             if (!zipCode.matches("([0-9]{5})|([0-9]{5}-[0-9]{4})"))
                 throw new FormValidationException("invalid zip code");
             if (Integer.valueOf(zipCode)<10000 || Integer.valueOf(zipCode)>99999)
-                throw new FormValidationException("length should exactly equal to 5");
+                throw new FormValidationException("zip code length should be exactly equal to 5");
         } catch (FormValidationException e) { return "invalid zip code"; }
 
          /* Check icdCode validation */
@@ -43,95 +48,133 @@ public class RequestBiosurveillanceAction {
         } catch (Exception e) { return "invalid diagnosis code"; }
 
         try {
-            Date start = new Date(date.getTime() - 1000*60*60*24*14);
+            Date start = new Date(date.getTime() - 14*DAY);
             diagnosisStatisticsBean = diagDAO.getWeeklyCounts(icdCode, zipCode, start, date);
         } catch (DBException e) {
             throw new ITrustException(e.getMessage());
         }
 
-        switch (icdCode) {
-            case "84.50":
-                return isMalariaEpidemic(diagnosisStatisticsBean, threshold)?"Yes":"No";
-            case "487.00":
-                return isInfluenzaEpidemic(diagnosisStatisticsBean)?"Yes":"No";
-            default:
-                return "No analysis can occur";
+        if(icdCode.length() >= 3 && icdCode.substring(0, 3).equals("84.")) {
+            return isMalariaEpidemic(diagnosisStatisticsBean, threshold) ? "Yes" : "No";
         }
+        else if(icdCode.length() >= 4 && icdCode.substring(0, 4).equals("487.")) {
+            return isInfluenzaEpidemic(diagnosisStatisticsBean) ? "Yes" : "No";
+        }
+        return "No analysis can occur";
     }
 
-    /* return the order of the week of the date from 1/1 in same year */
-    private long weekNumber(Date date) {
-        Date startDateOfYear = new Date(date.getYear(),1,1);
-        return (date.getTime() - startDateOfYear.getTime())/(1000 * 60 * 60 * 24 * 7) + 1;
+    /**
+     * Determine the number of weeks that have passed since Jan. 1st until (date).
+     * @param date
+     * @return Week of the year.
+     */
+    public long weekNumber(Date date) {
+        Date startDateOfYear = new Date(date.getYear(),0,1);
+        return (date.getTime() - startDateOfYear.getTime())/(7*DAY) + 1;
     }
 
+    /**
+     * Determine if there is a Malaria epidemic.
+     * @param diagnosisStatistics List of DiagnosisStatisticsBeans.
+     * @param threshold Threshold proportion: i.e. 1.0 is normal, 0.5 is twice as likely to report epidemic.
+     * @return True if there are at least 2 consecutive weeks of Malaria count over the average * threshold.
+     * @throws DBException
+     */
     private boolean isMalariaEpidemic(ArrayList<DiagnosisStatisticsBean> diagnosisStatistics, double threshold) throws DBException {
         if (diagnosisStatistics.size()<2)
             return false;
 
         Date earliest = diagDAO.findEarliestIncident("84.");
-        double [] average;
+        ArrayList<Double> averages;
         if(earliest != null) {
             double yearCount = 0;
-            long[] diagnosisCount = new long[]{0, 0};
+            ArrayList<Long> malariaSums = new ArrayList<>(0);
+            for(int i = 0; i < diagnosisStatistics.size(); i++) { //init
+                malariaSums.add(i, 0L);
+            }
+
             if (!isEarlierMonth(earliest, diagnosisStatistics.get(0).getStartDate())) {
+                //If month of earliest case is after month interested in, start averages with next year.
                 earliest.setYear(earliest.getYear() + 1);
             }
 
+            // Get averages of all weeks interested in for all years prior to the current year.
             Date startDate = new Date(diagnosisStatistics.get(0).getStartDate().getTime());
             startDate.setYear(earliest.getYear());
             Date endDate = new Date(diagnosisStatistics.get(1).getEndDate().getTime());
             endDate.setYear(earliest.getYear());
 
             while (startDate.before(diagnosisStatistics.get(0).getStartDate())) {
-
                 ArrayList<DiagnosisStatisticsBean> yearResult = diagDAO.getWeeklyCounts("84.50", diagnosisStatistics.get(0).getZipCode(), startDate, endDate);
                 startDate.setYear(startDate.getYear() + 1);
                 endDate.setYear(endDate.getYear() + 1);
 
                 yearCount++;
                 for (int i = 0; i < yearResult.size(); i++) {
-                    diagnosisCount[i] += yearResult.get(i).getRegionStats();
+                    malariaSums.set(i, malariaSums.get(i) + yearResult.get(i).getRegionStats());
                 }
             }
             if(yearCount == 0) { // date we're checking is before first case of malaria, so can't be epidemic
                 return false;
             }
 
-            // calculate the average
-            average = new double[] { diagnosisCount[0]/yearCount, diagnosisCount[1]/yearCount };
+            // Calculate weekly averages
+            averages = new ArrayList<>(0);
+            for(int i = 0; i < malariaSums.size(); i++) {
+                averages.add(i, malariaSums.get(i) / yearCount);
+            }
         } else { // no cases on record
-            average = new double[] {0., 0.};
+            return false;
         }
 
-
-        if(diagnosisStatistics.get(0).getRegionStats() > threshold * average[0])
-            if(diagnosisStatistics.get(1).getRegionStats() > threshold * average[1])
-                return true;
+        //Finally, determine if there are 2 consecutive weeks over average, taking threshold into account.
+        boolean lastWeekOverThreshold = false;
+        for(int i = 0; i < diagnosisStatistics.size(); i++) {
+            if(diagnosisStatistics.get(i).getRegionStats() > threshold * averages.get(i)) {
+                if(lastWeekOverThreshold) return true; //prev. week was over threshold - 2 consecutive weeks
+                lastWeekOverThreshold = true;
+            }
+        }
 
         return false;
     }
 
+    /**
+     * Determine if (first) date is earlier or in the same week of the year as (second).
+     * @param first Date.
+     * @param second Date.
+     * @return True if so.
+     */
     private boolean isEarlierMonth (Date first, Date second) {
         return weekNumber(first) <= weekNumber(second);
     }
 
+    /**
+     * Determine if there is an Influenza epidemic.
+     * @param diagnosisStatistics List of DiagnosisStatisticsBeans.
+     * @return True if there are at least 2 consecutive weeks of Influenza count over the threshold.
+     */
     private boolean isInfluenzaEpidemic(ArrayList<DiagnosisStatisticsBean> diagnosisStatistics) {
         if (diagnosisStatistics.size()<2)
             return false;
 
-        long regionCount = diagnosisStatistics.get(0).getRegionStats();
-
-        if(regionCount > calcThreshold(weekNumber(diagnosisStatistics.get(0).getStartDate())))
-        {
-            regionCount = diagnosisStatistics.get(1).getRegionStats();
-            if(regionCount > calcThreshold(weekNumber(diagnosisStatistics.get(1).getStartDate()))){
-                return true;
+        boolean lastWeekOverThreshold = false;
+        for(int i = 0; i < diagnosisStatistics.size(); i++) {
+            double weekThreshold = calcThreshold(weekNumber(diagnosisStatistics.get(0).getStartDate()));
+            if(diagnosisStatistics.get(i).getRegionStats() > weekThreshold) {
+                if(lastWeekOverThreshold) return true; //prev. week was over threshold - 2 consecutive weeks
+                lastWeekOverThreshold = true;
             }
         }
+
         return false;
     }
 
+    /**
+     * Determine the Influenza epidemic threshold.
+     * @param weekNumber A given week of the year.
+     * @return Threshold.
+     */
     private double calcThreshold(double weekNumber) {
         return 5.34 + 0.271 * weekNumber + 3.45 * Math.sin(2 * Math.PI * weekNumber / 52.0)
                 + 8.41 * Math.cos(2 * Math.PI * weekNumber / 52.0);
--- iTrust/test/edu/ncsu/csc/itrust/unit/action/RequestBiosurveillanceActionTest.java
+++ iTrust/test/edu/ncsu/csc/itrust/unit/action/RequestBiosurveillanceActionTest.java
@@ -10,35 +10,17 @@ import edu.ncsu.csc.itrust.unit.datagenerators.TestDataGenerator;
 import edu.ncsu.csc.itrust.unit.testutils.TestDAOFactory;
 import junit.framework.TestCase;
 
+import java.sql.Time;
+import java.util.Calendar;
 import java.util.Date;
 
 public class RequestBiosurveillanceActionTest extends TestCase {
-    private static int visitIdCount = 0;
     private final String MALARIA = "84.50";
     private final String INFLUENZA = "487.00";
 
     private TestDataGenerator gen;
     private RequestBiosurveillanceAction action;
     private DAOFactory factory = TestDAOFactory.getTestInstance();
-    private DiagnosesDAO diagnosesDAO;
-    private OfficeVisitDAO officeVisitDAO;
-
-    private class Sickness {
-        public OfficeVisitBean visit;
-        public DiagnosisBean diagnosis;
-        public Sickness(String icdCode, String date, long patientId) {
-            diagnosis = new DiagnosisBean();
-            diagnosis.setICDCode(icdCode);
-            diagnosis.setVisitID(visitIdCount);
-            visit = new OfficeVisitBean(visitIdCount);
-            visit.setVisitDateStr(date);
-            visit.setHcpID(9000000000L);
-            visit.setNotes("Diagnose Influenza");
-            visit.setPatientID(patientId);
-            visit.setHospitalID("1");
-            visitIdCount++;
-        }
-    }
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -46,65 +28,72 @@ public class RequestBiosurveillanceActionTest extends TestCase {
         gen.clearAllTables();
         gen.standardData();
         action = new RequestBiosurveillanceAction(factory);
-        diagnosesDAO = factory.getDiagnosesDAO();
-        officeVisitDAO = factory.getOfficeVisitDAO();
     }
 
     public void testDetectEpidemic_ZipCode() throws Exception{
-
         assertEquals("invalid zip code", action.detectEpidemic("anything", "ABCDE", new Date(),.0));
         assertEquals("invalid zip code", action.detectEpidemic("anything", "1234", new Date(),.0));
         assertEquals("invalid zip code", action.detectEpidemic("anything", "01234", new Date(),.0));
         assertEquals("invalid zip code", action.detectEpidemic("anything", "1A234", new Date(),.0));
-        //assertEquals(null, action.detectEpidemic("84.50", "13234", new Date(),.0));
     }
 
     public void testDetectEpidemic_IcdCode() throws Exception {
-
         assertEquals("invalid diagnosis code", action.detectEpidemic("anything", "12334", new Date(),.0));
         assertEquals("No analysis can occur", action.detectEpidemic("12.3", "12334", new Date(),.0));
         assertEquals("No analysis can occur", action.detectEpidemic("90", "12334", new Date(),.0));
-
     }
 
     public void testWeekNumber() {
-       // assertEquals(45, action.weekNumber(new Date(2017,11,9)));
+        assertEquals(3, action.weekNumber(new Date(2017, 0, 16)));
+        assertEquals(45, action.weekNumber(new Date(2017,10,9)));
     }
 
     public void testDetectEpidemic() throws Exception {
-        isInfluenzaEpidemic_test();
-        isMalariaEpidemic_test();
+        Date presentCases = new Date(new Date().getYear(),10, 9);
+        isInfluenzaEpidemic_test(presentCases);
+        isMalariaEpidemic_test(presentCases);
     }
 
-    private void isInfluenzaEpidemic_test() throws Exception {
-
-
-    }
-
-    private void isMalariaEpidemic_test() throws  Exception {
-        Sickness[] sicknesses = new Sickness[3];
-        sicknesses[0] = new Sickness(MALARIA, "01/03/2010",1);
-        sicknesses[1] = new Sickness(MALARIA, "01/06/2011", 1);
-        sicknesses[2] = new Sickness(MALARIA, "01/05/2012", 1);
-        gen.clearAllTables();
-        gen.patient1();
-
+    private void isInfluenzaEpidemic_test(Date presentCases) throws Exception {
         //Test 1: Empty DB, not epidemic
-        assertEquals("No", action.detectEpidemic(MALARIA, "27606", sicknesses[2].visit.getVisitDate(), 1.0));
+        gen.clearAllTables();
+        assertEquals("No", action.detectEpidemic(INFLUENZA, "27606",
+                presentCases , 0.));
 
-        //Add 3 cases to DB
-        for(int i = 0; i < sicknesses.length; i++) {
-            diagnosesDAO.add(sicknesses[i].diagnosis);
-            officeVisitDAO.add(sicknesses[i].visit);
-        }
+        //Test 2: No influenza data, not epidemic
+        gen.standardData();
+        assertEquals("No", action.detectEpidemic(INFLUENZA, "27606",
+                presentCases , 0.));
 
-        //Test 2: Populated, not epidemic
-        assertEquals("No", action.detectEpidemic(MALARIA, "27606", sicknesses[2].visit.getVisitDate(), 1.0));
-
-        //Test 3: Epidemic
-        Sickness epidemic = new Sickness(MALARIA, "01/04/2012",1);
-        diagnosesDAO.add(epidemic.diagnosis);
-        officeVisitDAO.add(epidemic.visit);
-        assertEquals("Yes", action.detectEpidemic(MALARIA, "27606", sicknesses[2].visit.getVisitDate(), 0.9));
+        //Test 3: Populated, epidemic
+        gen.influenza_epidemic();
+        assertEquals("Yes", action.detectEpidemic(INFLUENZA, "27606",
+                presentCases , 0.));
     }
+
+    private void isMalariaEpidemic_test(Date presentCases) throws  Exception {
+        //Test 1: Empty DB, not epidemic
+        gen.clearAllTables();
+        assertEquals("No", action.detectEpidemic(MALARIA, "27606",
+               presentCases , 0.1));
+
+        //Test 2: No malaria data, not epidemic
+        gen.standardData();
+        assertEquals("No", action.detectEpidemic(MALARIA, "27606",
+                presentCases , 0.1));
+
+        //Test 3: Populated, epidemic
+        gen.malaria_epidemic();
+        assertEquals("Yes", action.detectEpidemic(MALARIA, "27606",
+                presentCases , 1.0));
+
+        //Test 4: Show threshold yields correct results: still epidemic for < 5
+        assertEquals("Yes", action.detectEpidemic(MALARIA, "27606",
+                presentCases , 4.9));
+        assertEquals("Yes", action.detectEpidemic(MALARIA, "27606",
+                presentCases , 5.0));
+
+    }
+
+
 }
